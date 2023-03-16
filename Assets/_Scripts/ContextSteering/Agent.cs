f2fe649ad9d78d97ae3162fb6372eff6574380f7
alennacayton <cayton.alenna@gmail.com>
2023-03-16 14:45:08 +0800

    Initial Commit

---
+++ Assets/_Scripts/ContextSteering/Agent.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    private AgentAnimations agentAnimations;
+    private AgentMover agentMover;
+
+
+    private Vector2 pointerInput, movementInput;
+
+    public Vector2 PointerInput { get => pointerInput; set => pointerInput = value; }
+    public Vector2 MovementInput { get => movementInput; set => movementInput = value; }
+
+    private void Update()
+    {
+        //pointerInput = GetPointerInput();
+        //movementInput = movement.action.ReadValue<Vector2>().normalized;
+
+        agentMover.MovementInput = MovementInput;
+        AnimateCharacter();
+    }
+
+ 
+
+    private void Awake()
+    {
+        agentAnimations = GetComponentInChildren<AgentAnimations>();
+        agentMover = GetComponent<AgentMover>();
+    }
+
+    private void AnimateCharacter()
+    {
+        Vector2 lookDirection = pointerInput - (Vector2)transform.position;
+        agentAnimations.RotateToPointer(lookDirection);
+        agentAnimations.PlayAnimation(MovementInput);
+    }
+
+    
+
+}
