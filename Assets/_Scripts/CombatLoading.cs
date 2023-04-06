c245e20d7664e329aa6ef26ee3762150b3fb70b5
Aki <57061301+akiescarraga@users.noreply.github.com>
2023-04-06 20:22:42 +0800

    finished functionality of title screen and loading of screens to next level

---
+++ Assets/_Scripts/CombatLoading.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CombatLoading : MonoBehaviour
+{
+    public void ProceedtoLevelTwo()
+    {
+        //SceneManager.LoadScene("Level2");
+    }
+
+    public void ProceedtoLevelThree()
+    {
+        //SceneManager.LoadScene("Level2");
+    }
+
+    public void ProceedtoLevelFour()
+    {
+        //SceneManager.LoadScene("Level2");
+    }
+
+    public void FinishGame()
+    {
+        //SceneManager.LoadScene("End");
+    }
+}
