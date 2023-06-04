bc0d8f49ca747602bd226e49d0b18d4d8004bf71
DanielBaguio <daniel_baguio@dlsu.edu.ph>
2023-06-04 16:52:22 +0800

    Added more features to the combat system
    Retry page was added and other features but currently there are two bugs that needs to be fixed

--- Assets/_Scripts/CombatSystem/QuizManager.cs
+++ Assets/_Scripts/CombatSystem/QuizManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
@@ -10,21 +11,21 @@ public class QuizManager : MonoBehaviour
     public List<QuestionsAndAnswers> QnA;
     public GameObject[] options;
     public int currentQuestion;
-    
+
     public GameObject quizPanel;
     public GameObject goPanel;
 
-    public Text questionTxt;
+    public TMP_Text questionTxt;
+    public Text scoreTxt;
+
+    int totalQuestions = 0;
+    public int scoreCount;
+    public string score;
 
     private void start()
     {
+        totalQuestions = QnA.Count;
         goPanel.SetActive(false);
-        generateQuestion();
-    }
-
-    public void correct()
-    {
-        QnA.RemoveAt(currentQuestion);
         generateQuestion();
     }
 
@@ -32,6 +33,7 @@ public class QuizManager : MonoBehaviour
     {
         quizPanel.SetActive(false);
         goPanel.SetActive(true);
+        //scoreTxt.txt = (scoreCount.ToString()) + "/" + (totalQuestions.ToString());
     }
 
     public void retry()
@@ -39,12 +41,25 @@ public class QuizManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    public void correct()
+    {
+        scoreCount += 1;
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    public void wrong()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
     void setAnswers()
     {
         for (int i = 0; i < options.Length; i++)
         {
             options[i].GetComponent<AnswerScript>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].answers[i];
+            options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].answers[i];
 
             if(QnA[currentQuestion].correctAnswer == i+1)
             {
@@ -55,18 +70,21 @@ public class QuizManager : MonoBehaviour
 
     void generateQuestion()
     {
-        if(QnA.Count > 0)
-        {
-        currentQuestion = Random.Range(0, QnA.Count);
 
-        questionTxt.text = QnA[currentQuestion].question;
-        setAnswers();
+        if (QnA.Count > 0)
+        {
+            currentQuestion = UnityEngine.Random.Range(0, QnA.Count);
+
+            questionTxt.text = QnA[currentQuestion].question;
+            setAnswers();
         }
         else
         {
-            Debug.Log("Out of questions.");
+            Debug.Log("Out of questions");
             gameOver();
         }
+
+
 
     }
 
